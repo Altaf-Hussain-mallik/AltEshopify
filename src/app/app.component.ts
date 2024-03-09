0606df44f1dcd197fec7b93f8146100bab880c3f
Altaf Hussain <altafraja00711@gmail.com>
2024-03-09 16:37:46 +0530

    added pages file and folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterLinkActive,
+    RouterOutlet,
+    RouterLink,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  { path: 'home', title: 'Home', component: HomeComponent },
+  { path: 'services', title: 'Service', component: ServicesComponent },
+  { path: 'technologies', title: 'Technologies', component: TechnologiesComponent },
+  { path: 'portfolio', title: 'Portfolio', component: PortfolioComponent },
+  { path: 'company', title: 'Company', component: CompanyComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
+  // Add more routes as needed
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
