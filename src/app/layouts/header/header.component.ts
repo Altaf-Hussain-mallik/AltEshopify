1e1727055708964fc56898941524d7035e8e59a5
Altaf Hussain <altafraja00711@gmail.com>
2024-03-12 03:44:15 +0530

    sticky header desktop view

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -9,5 +9,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isHeaderVisible: boolean = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.isHeaderVisible = (window.pageYOffset > 100);
+  }
 }
